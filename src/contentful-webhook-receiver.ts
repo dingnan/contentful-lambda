1ddacb3f298cf8c62ec86f0206f252987486f6c5
Ding Nan <nandingb@gmail.com>
2026-06-22 18:17:31 -0400

    refactor: improve logging message format in webhook receiver

--- src/contentful-webhook-receiver.ts
+++ src/contentful-webhook-receiver.ts
@@ -1,5 +1,5 @@
 exports.handler = async (event, context) => {
-  console.log('😀Hello World - Contentful webhook received payload:', {event, context});
+  console.log('😀 Contentful webhook received payload:', {event, context});
 
   return {
     statusCode: 200,
